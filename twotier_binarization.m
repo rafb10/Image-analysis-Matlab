ae2b3d7e5efeec4a3e9869b4b49040e1ff6adb5e
rafb10 <38700521+rafb10@users.noreply.github.com>
2018-05-27 20:15:13 +0200

    Update twotier_binarization.m

--- twotier_binarization.m
+++ twotier_binarization.m
@@ -16,12 +16,13 @@ for i = 1:Xsize
       for j = 1:Ysize
 
                 if(image1(i,j)> 190 & image1(i,j)< 215)
-                    bartBW(i,j) = image1(i,j);
+                     image1(i,j) = 255;
+                else
+                     image1(i,j) = 0;
          
                 end
       end          
 end
 
-figure(2)
-bartB = uint8(bartBW);
+figure(2);
 imshow(bartB);
